076e58f13df3817642c14ebd4663e5bb27344c2d
RITIK KANSAL <ritikkansal5@gmail.com>
2026-01-19 22:21:05 +0530

    task 3

--- server/index.js
+++ server/index.js
@@ -11,8 +11,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use("/api", router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const connectDB = async () => {
   try {
